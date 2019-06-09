50e2f670292e8302857a12e584973940d5c8e152
AlanSalas <dubekone@gmail.com>
2019-06-09 09:28:42 -0500

    Ejercicios prueba_tecnica

---
+++ prueba_3.php
@@ -0,0 +1,11 @@
+<?php
+    echo "<center>";
+    //Imprimir 5 lineas PRUEBA 1, 2, 3, 4, 5.
+    for ($i=1; $i < 6; $i++) { 
+        echo "PRUEBA ".$i; echo "<br>";
+        echo "<br>";
+    }
+
+    echo"<img src='https://picsum.photos/id/1041/400/400' alt='prueba#4'>";
+    echo "</center>";
+?>
